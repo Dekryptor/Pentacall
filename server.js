861ff20e586172c118e321729c56cb5511987373
Sebastian Streich <streich.mobile@gmail.com>
2016-04-28 13:44:33 +0200

    First Commit
    Lots of Stuff happened here already.

---
+++ server.js
@@ -0,0 +1,37 @@
+/*
+RIFT CALLING SYSTEM
+
+
+MIT License
+Sebastian Streich
+*/
+
+
+var express = require("express");
+var fs = require("fs")
+var peerProvider = require('peer').ExpressPeerServer;
+var callBroker = require("./lib/callBroker.js")
+
+var app = express();
+
+//Try to Read the Secret Config File
+var config = JSON.parse(fs.readFileSync("config.json",{encoding:"utf-8"}));
+
+
+
+// Lets Initialise the static HTTP Server, pretty boring
+app.use(express.static('www'));
+var server = app.listen(3000);
+
+ /*
+ In this APP Peer.js only Handles Interconnecting the VOIP Clients, 
+ the Controllflow is completely Handled by a Websocket.
+ */
+    //Lets listen the PeerJS Broker on /voip
+    app.use("/voip",peerProvider(server,{debug:true}));
+
+    //Initialise the socket IO Instance 
+    var io      = require("socket.io")(server);
+    io.on("connection",function(socket) {
+        callBroker.register(socket);
+    });
---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+var d;
+var s;
+window.addEventListener("load",function() {
+    console.log("READY");
+    var socket = io();
+    var peer = new Peer(null,{host:"localhost",port:"3000",path:"/voip",debug:3});
+    //TODO: DEBUG STUFF, PLZ REMOVE if done
+        s = socket;
+        d= peer;
+    
+    document.querySelector("paper-button").addEventListener("click",function(e) {
+        //Enter Button Clicked, so lets register
+        var server = document.querySelectorAll("#loginForm paper-input")[0].value.toLowerCase();
+        var name = document.querySelectorAll("#loginForm paper-input")[1].value;
+        
+        var registerBlob={server:server,summonerName:name,callID:d.id};
+        socket.emit("register",registerBlob,onRegisterResponse); 
+    });
+    
+    function onRegisterResponse(ack,msg){
+           //We got an Answer to the Register Try. 
+            if(ack){
+                //We got in.
+                console.log("Gotcha");
+                var profiles = JSON.parse(msg);
+                var selector = Object.keys(profiles)[0];
+                
+                //Lets Prepare the UI:
+                var controller = document.querySelector("user-card");
+                controller.name=profiles[selector].name;
+                controller.level=profiles[selector].summonerLevel;
+                controller.icon=profiles[selector].profileIconId;
+                              
+                document.querySelector("#stateRouter").selected=1; // Switch to the other State
+            }
+            else{
+                //Nope -> Some Error here.
+                console.log("Fehler -> Reason:" +msg );
+            }
+       }; 
+    
+    socket.on("mateFound",function(data) {
+        //We got another Member so, lets call Him
+        
+    });
+    
+    
+});
