2a0150733462070292e1bfbed3ef2f2321a8fa20
Sebastian Streich <streich.mobile@gmail.com>
2016-04-29 03:53:27 +0200

    Front End does now Check Requirements

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,156 @@
 var d;
 var s;
 window.addEventListener("load",function() {
-    console.log("READY");
     var socket = io();
-    var peer = new Peer(null,{host:"localhost",port:"3000",path:"/voip",debug:3});
-    //TODO: DEBUG STUFF, PLZ REMOVE if done
-        s = socket;
-        d= peer;
-    
-    document.querySelector("paper-button").addEventListener("click",function(e) {
-        //Enter Button Clicked, so lets handle
-        var server = document.querySelectorAll("#loginForm paper-input")[0].value.toLowerCase();
-        var name = document.querySelectorAll("#loginForm paper-input")[1].value;
-        
-        var registerBlob={server:server,summonerName:name,callID:d.id};
-        socket.emit("register",registerBlob,onRegisterResponse);
-    });
-    
-    function onRegisterResponse(ack,msg){
-           //We got an Answer to the Register Try. 
-            if(ack){
-                //We got in.
-                console.log("Gotcha");
-                var user = msg;
+    var peer = new Peer(null, {host: "localhost", port: "3000", path: "/voip", debug: 3});
+    var toast = document.querySelector("paper-toast");
+    var mediaStream;
+
+    var pages = document.querySelector("iron-pages");
+
+    async.waterfall([
+        function (doneCallback) {
+            //Stage 0: Check and Bootup
+            var root    = document.querySelector("#boot");
+            var heading = root.querySelector("h1");
+            var text    = root.querySelector("p");
+            var btn     = root.querySelector("paper-button");
+            var logo    = root.querySelector("img");
+            
+            function show(e) {e.setAttribute("class","");}
+
+            async.waterfall([
+                //Into and show everything
+                function (callback) {
+                    //Phase 1, show the logo and
+                    logo.setAttribute("class","animated  zoomIn");
+                    setTimeout(function () {
+                        callback(null);
+                    },1000);
+                },
+                function (callback) {
+                    //Check if the Browser Supports Calling.
+                    if(util.supports.audioVideo){callback(null)}
+                    else{callback({heading:"We're really Sorry",text:"But it seems Like your Browser doesnt Support WebRTC, which is required."})}
+                },
+                function (callback) {
+                    //Then Lets Get a Media Stream.
+
+                    navigator.getUserMedia  = navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia;
+
+                    navigator.getUserMedia({audio: true},function (stream) {
+                        mediaStream=stream;
+                        callback(null)
+                    },function () {
+                        callback({heading:"Its silent here.",text:"To enable Calling, we need ascess to your Microphone. Please change your Settings and reload the Page."})
+                    });
+
+                },
+                function (callback) {
+                    //Last Check. Lets see if The Sockets are up.
+                    if(socket.connected && peer.id != null){
+                        callback(null);
+                    }
+                    else{
+                        callback({heading:"Ooops. Something went wrong.",text:"We couldn't establish a Connection to the Server, we're really sorry."})
+                    }
+
+
+                }
                 
-                //Lets Prepare the UI:
-                var controller = document.querySelector("user-card");
-                controller.name=user.name;
-                controller.level=user.summonerLevel;
-                controller.icon=user.profileIconId;
-                              
-                document.querySelector("#stateRouter").selected=1; // Switch to the other State
+            ],function(err,res){
+                //Here Happened Some Error, so Lets print it.
+                if(err){
+                    heading.innerText=err.heading;
+                    text.innerText=err.text;
+                    show(heading);
+                    show(text);
+                }else{
+                    //No Errors, so we're good to go! <3
+                    console.log("All checks done. We're ready to rumble.");
+                    pages.selected++;
+                    doneCallback(null);
+                }
+            });
+
+
+
+        },
+        function (doneCallback) {
+            //Stage 1 Login:
+            var root = document.querySelector("#loginState");
+
+            //Get the Old Stage, if exists.
+            var name = localStorage.getItem("summonerName") || "";
+            var region = localStorage.getItem("region") || 0;
+
+            var serverField = root.querySelectorAll("paper-input")[0];
+            var nameField = root.querySelectorAll("paper-input")[1];
+            var connectButton = root.querySelector("paper-button")
+            //Restore.
+            serverField.selected = region;
+            nameField.value = name;
+
+
+            function buttonClick(){
+                //Get the Values
+                name = nameField.value;
+                if(name.length<4){validationError("Name too short");return;}
+                var server = serverField.value.toLowerCase();
+                var registerBlob = {server: server, summonerName: name, callID: peer.id};
+                socket.emit("register", registerBlob, onRegisterResponse);
             }
-            else{
-                //Nope -> Some Error here.
-                console.log("Fehler -> Reason:" +msg );
+
+            function validationError(msg) {
+                toast.text=msg;
+                toast.open();
+                //Let the Form Shake for some Time
+                root.querySelector("div").setAttribute("class", "animated shake infinite");
+                window.setTimeout(function () {
+                    root.querySelector("div").setAttribute("class", "");
+                }, 500);
             }
-       }; 
-    
-    socket.on("mateFound",function(data) {
-        //We got another Member so, lets call Him
-        console.log(data);
+
+            function onRegisterResponse(sucsess, msg) {
+                if (sucsess) {
+                    localStorage.setItem("summonerName",name);
+                    localStorage.setItem("region",serverField.selected);
+                    connectButton.removeEventListener("click",buttonClick);
+                    doneCallback(null,msg);
+                }
+                else {
+                    validationError(msg);
+                }
+            }
+
+            connectButton.addEventListener("click", buttonClick);
+
+        },
+
+        function (User) {
+            //Stage 2:
+
+
+
+
+        }
+
+    ]);
+
+    //Enable Protocoll Awareness:
+
+    socket.on("mateFound",function () {
+
     });
-    
-    
+
+
+
+
+
 });
+
+
+
